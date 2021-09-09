80aec71f28b9e9d801592fa954231322c563351e
Hinano <ubuntu@ip-172-31-14-36.ap-northeast-1.compute.internal>
2021-09-09 10:48:39 +0000

    login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to :login
+      redirect_to @user
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
