a4833a0721c7187df457725b0635c4fdd8b2c556
Hinano <ubuntu@ip-172-31-14-36.ap-northeast-1.compute.internal>
2021-09-12 06:55:37 +0000

    search

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -3,9 +3,9 @@ class Shop < ApplicationRecord
   
   validates :name, presence: true, length: { maximum: 50 }
   validates :address, allow_blank: true, length: { maximum: 50 }
-  validates :opening_hours, numericality: { only_integer: true },allow_blank: true
+  validates :opening_hours, length: { maximum: 50 }, allow_blank: true
   validates :regular_holiday, length: { maximum: 50 }, allow_blank: true
-  validates :floor, numericality: { only_integer: true },allow_blank: true
+  validates :floor, length: { maximum: 50 }, allow_blank: true
   validates :phone_number,  numericality: { only_integer: true },allow_blank: true, format: {with: /\A\d{10,11}\z/}
   validates :category, length: { maximum: 20 }, allow_blank: true
   validates :payment_method, length: { maximum: 50 }, allow_blank: true
