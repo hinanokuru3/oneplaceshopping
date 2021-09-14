daa5f6b3868205807de39e12fabd92eb77e7d899
Hinano <ubuntu@ip-172-31-14-36.ap-northeast-1.compute.internal>
2021-09-14 10:26:54 +0000

    search

--- app/controllers/shopping_centers_controller.rb
+++ app/controllers/shopping_centers_controller.rb
@@ -2,19 +2,16 @@ class ShoppingCentersController < ApplicationController
   #def index
     #@shopping_centers=ShoppingCenter.all
   #end
-  before_action :shop, only: [:search]
   def show
     @shopping_center=ShoppingCenter.find(params[:id])
   end
   
   def search
-    @shopping_center=shop.shopping_center
+    @shop=Shop.where("name LIKE ?", "#{params[:name1]}")
+    @shopping_center=@shop.shopping_center
   end
 
   private
   
-  def shop
-    @shop=Shop.where("name LIKE ?", "#{params[:name1]}")
-  end
-
+ 
 end
