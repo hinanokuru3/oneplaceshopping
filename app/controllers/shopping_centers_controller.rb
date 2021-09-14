72c2f7a2ebc17ead76295c46b7f382d784cf5830
Hinano <ubuntu@ip-172-31-14-36.ap-northeast-1.compute.internal>
2021-09-14 11:47:43 +0000

    search

--- app/controllers/shopping_centers_controller.rb
+++ app/controllers/shopping_centers_controller.rb
@@ -8,7 +8,7 @@ class ShoppingCentersController < ApplicationController
   
   def search
     #@shop=Shop.where("name LIKE ?", "#{params[:name1]}")
-    @shopping_center=ShoppingCenter.joins(:shops).where(shops: {name: params[:name1]})
+    @shopping_center=ShoppingCenter.joins(:shops).where(shops: {name: params[:name1]}) and where(shops: {name: params[:name2]}) and where(shops: {name: params[:name3]})
   end
 
   private
