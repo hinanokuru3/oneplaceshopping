5fa4145b21f87e4e1fa470e7955e178c4d88208c
Hinano <ubuntu@ip-172-31-14-36.ap-northeast-1.compute.internal>
2021-09-25 03:14:03 +0000

    rails

--- app/controllers/shopping_centers_controller.rb
+++ app/controllers/shopping_centers_controller.rb
@@ -6,6 +6,23 @@ class ShoppingCentersController < ApplicationController
     @shopping_center=ShoppingCenter.find(params[:id])
   end
   
+  
+  def shopping_center_history #閲覧履歴
+    @history = ShoppingCenter.browsing_history(user)
+  end
+  
+  
+  def search
+    @shopping_center=ShoppingCenter.search_by_keywords(search_keywords)
+  end
+
+  private
+  
+  def search_keywords
+    [params[:name1], params[:name2], params[:name3]].reject(&:blank?)
+    
+  end
+  
   def browsing_history(user)
     if logged_in?
       new_history = histories.new
@@ -17,21 +34,6 @@ class ShoppingCentersController < ApplicationController
       end
       new_history.save
     end
-  end
-  
-  def shopping_center_history #閲覧履歴
-    @history = BrowsingHistory.all
-  end
-  
-  def search
-    @shopping_center=ShoppingCenter.search_by_keywords(search_keywords)
-  end
-
-  private
-  
-  def search_keywords
-    [params[:name1], params[:name2], params[:name3]].reject(&:blank?)
-    
   end
   
  
