a58cbc430467f6732c5c8d194059038aa9ed3fe7
Hinano <ubuntu@ip-172-31-14-36.ap-northeast-1.compute.internal>
2021-09-25 03:33:06 +0000

    rails

--- app/controllers/shopping_centers_controller.rb
+++ app/controllers/shopping_centers_controller.rb
@@ -4,11 +4,25 @@ class ShoppingCentersController < ApplicationController
 
   def show
     @shopping_center=ShoppingCenter.find(params[:id])
+   
+    #ここから閲覧履歴を保存するコード
+    new_history = @shopping_center.histories.new
+    new_history.user_id = current_user.id
+
+    #if文追加、ログイン中のユーザー(current_user)の閲覧履歴(browsing_histories)の中で記事ID(article_id)が
+    #URLに入力されているID(params[:id])と同じものがすでに存在しているか？(exists?)
+    if current_user.histories.exists?(shopping_center_id: "#{params[:id]}")
+      old_history = current_user.histories.find_by(shopping_center_id: "#{params[:id]}")
+      old_history.destroy
+    end
+
+    new_history.save
+
   end
   
   
   def shopping_center_history #閲覧履歴
-    @history = ShoppingCenter.browsing_history(user)
+    @history = History.all
   end
   
   
